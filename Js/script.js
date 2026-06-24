3a22557e39a67aec328d8a602c1fd35d77d51ce5
Shahendaa <shahenda_alii@outlook.com>
2026-06-24 08:19:55 +0300

    Add toggle icon for mobile menu

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+
+    navMenu.classList.toggle('active');
+
+    if (navMenu.classList.contains('active')) {
+        menuToggle.innerHTML = '✕';
+    } else {
+        menuToggle.innerHTML = '☰';
+    }
+
+});
